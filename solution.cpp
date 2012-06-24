f19ac112e772736b482f6447f88f31d92e7a1412
rshepherd <me@rshepherd.org>
2012-06-23 22:33:07 -0400

    Fixing whacky formatting

--- solution.cpp
+++ solution.cpp
@@ -29,48 +29,43 @@ void parseFile(ifstream& inFile, vector<vector<unsigned> >& lines);
 // Returns the sum of the optimal path through the triangle
 unsigned calculate(vector<vector<unsigned> >& lines);
 
-int main()
-{
-	ifstream inFile;
-	inFile.open(INPUT_FILE_NAME.c_str());
+int main() {
+  ifstream inFile;
+  inFile.open(INPUT_FILE_NAME.c_str());
 
-	// 1D=lines 2D=numbers in each line
-	vector<vector<unsigned> > lines;
-	parseFile(inFile, lines);
+  // 1D=lines 2D=numbers in each line
+  vector<vector<unsigned> > lines;
+  parseFile(inFile, lines);
 
-	cout << calculate(lines) << endl;
+  cout << calculate(lines) << endl;
 }
 
-void parseFile(ifstream& inFile, vector<vector<unsigned> >& lines)
-{
-	string line;
-	unsigned number;
-	while (getline(inFile, line)) {
-		lines.push_back(vector<unsigned> ());
-		istringstream stringStream(line);
-		while (stringStream >> number)
-			lines.back().push_back(number);
-	}
+void parseFile(ifstream& inFile, vector<vector<unsigned> >& lines) {
+  string line;
+  unsigned number;
+  while (getline(inFile, line)) {
+    lines.push_back(vector<unsigned> ());
+    istringstream stringStream(line);
+    while (stringStream >> number)
+      lines.back().push_back(number);
+  }
 
-	reverse(lines.begin(), lines.end());
+  reverse(lines.begin(), lines.end());
 }
 
-unsigned calculate(vector<vector<unsigned> >& lines)
-{
-	// Starting at the bottom of the triangle...
-	for (int k = 0; k < lines.size() - 1; ++k) {
-		vector<unsigned>& current = lines.at(k);
-		vector<unsigned>& next = lines.at(k + 1);
+unsigned calculate(vector<vector<unsigned> >& lines) {
+  // Starting at the bottom of the triangle...
+  for (int k = 0; k < lines.size() - 1; ++k) {
+    vector<unsigned>& current = lines.at(k);
+    vector<unsigned>& next = lines.at(k + 1);
 
-		// ...evaluate each element's value relative to it's neighbor on the right.
-		// The greater is added to the shared adjacent element in the next line.
-		for (int i = 0; i < current.size() - 1; i++)
-			if (current[i] >= current[i + 1])
-				next[i] += current[i];
-			else
-				next[i] += current[i + 1];
-	}
+    // ...evaluate each element's value relative to it's neighbor on the right.
+    // The greater is added to the shared adjacent element in the next line.
+    for (int i = 0; i < current.size() - 1; i++)
+      if (current[i] >= current[i + 1]) next[i] += current[i];
+      else next[i] += current[i + 1];
+  }
 
-	// the new value at top of the triangle
-	return lines.back()[0];
+  // the new value at top of the triangle
+  return lines.back()[0];
 }
